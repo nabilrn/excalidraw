c8aa44a072a6a6291257de7f12fbbaebf1f89eda
Nabil <116415990+nabilrn@users.noreply.github.com>
2026-08-31 20:08:25 +0700

    feat: render homescreen calendar portal

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import "@excalidraw/excalidraw/index.css";
 import "./styles.css";
 import App from "./App";
+import { HomeCalendarPortal } from "./features/calendar/HomeCalendarPortal";
 import { UtilitySettings } from "./features/settings/UtilitySettings";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <HomeCalendarPortal />
     <UtilitySettings />
   </React.StrictMode>,
 );
